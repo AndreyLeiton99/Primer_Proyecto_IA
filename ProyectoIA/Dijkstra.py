1ef641b0443ebb772924c7f626889fe2b023a5ee
GuirsRB <guisrley.rodriguez.baltodano@est.una.ac.cr>
2023-08-27 21:14:24 -0600

    Diskstra

--- ProyectoIA/Dijkstra.py
+++ ProyectoIA/Dijkstra.py
@@ -1,1 +1,99 @@
-# Algoritmo de dijkstra
+import heapq
+import time
+
+# Definición de colores para resaltar los caminos en el laberinto
+COLORS = ['\033[91m', '\033[92m', '\033[93m', '\033[94m', '\033[95m', '\033[96m', '\033[97m']
+RESET_COLOR = '\033[0m'
+
+def dijkstra_laberinto(laberinto, inicio, meta):
+    n = len(laberinto)
+    m = len(laberinto[0])
+
+    distancias = [[float('inf')] * m for _ in range(n)]
+    distancias[inicio[0]][inicio[1]] = 0
+    visitados = [[False] * m for _ in range(n)]
+
+    cola_prioridad = [(0, inicio)]
+
+    while cola_prioridad:
+        distancia_actual, nodo_actual = heapq.heappop(cola_prioridad)
+        x, y = nodo_actual
+
+        if nodo_actual == meta:
+            break
+
+        if visitados[x][y]:
+            continue
+
+        visitados[x][y] = True
+
+        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            nx, ny = x + dx, y + dy
+
+            if 0 <= nx < n and 0 <= ny < m and not visitados[nx][ny]:
+                peso = 1 if laberinto[nx][ny] == 0 else float('inf')
+                nueva_distancia = distancia_actual + peso
+
+                if nueva_distancia < distancias[nx][ny]:
+                    distancias[nx][ny] = nueva_distancia
+                    heapq.heappush(cola_prioridad, (nueva_distancia, (nx, ny)))
+
+    ruta_minima = reconstruir_ruta(distancias, inicio, meta)
+    return ruta_minima
+
+
+def reconstruir_ruta(distancias, inicio, meta):
+    ruta = []
+    x, y = meta
+
+    while (x, y) != inicio:
+        ruta.append((x, y))
+        vecinos = []
+
+        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < len(distancias) and 0 <= ny < len(distancias[0]):
+                vecinos.append(((nx, ny), distancias[nx][ny]))
+
+        vecinos.sort(key=lambda v: v[1])
+
+        x, y = vecinos[0][0]
+    
+    ruta.append(inicio)
+    ruta.reverse()
+    return ruta
+
+def imprimir_laberinto_con_ruta(laberinto, ruta):
+    for i, fila in enumerate(laberinto):
+        for j, celda in enumerate(fila):
+            if (i, j) in ruta:
+                color_index = ruta.index((i, j)) % len(COLORS)
+                print(COLORS[color_index] + str(celda) + RESET_COLOR, end=' ')
+            else:
+                print(str(celda), end=' ')
+        print()
+
+# Crear el laberinto (cambiarlo por tu laberinto real)
+laberinto_ejemplo = [
+    [0, 1, 0, 0, 0],
+    [2, 0, 1, 1, 0],
+    [0, 0, 0, 1, 0],
+    [0, 1, 1, 0, 0],
+    [0, 0, 0, 0, 3]
+]
+
+inicio_ejemplo = (1, 0)
+meta_ejemplo = (4, 4)
+
+inicio_tiempo = time.time()
+ruta_minima = dijkstra_laberinto(
+    laberinto_ejemplo, inicio_ejemplo, meta_ejemplo)
+tiempo_total = time.time() - inicio_tiempo
+
+ruta_minima_str = ' -> '.join(f'({x},{y})' for x, y in ruta_minima)
+
+print("Ruta mínima:", ruta_minima_str)
+print("Tiempo total:", tiempo_total, "segundos")
+
+print("Laberinto con Ruta:")
+imprimir_laberinto_con_ruta(laberinto_ejemplo, ruta_minima)
--- ProyectoIA/ReadCSV.py
+++ ProyectoIA/ReadCSV.py
@@ -1,26 +1,37 @@
 import csv
 
+def cargar_laberinto(nombre_archivo):
+    laberinto = []
+    inicio = None
+    meta = None
 
-def load_maze_from_csv(filename,  delimiter=';'):
-    maze = []
-    with open(filename, 'r') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=delimiter)
-        for row in csv_reader:
-            maze_row = [int(cell) for cell in row]
-            maze.append(maze_row)
-    return maze
+    with open(nombre_archivo, 'r') as archivo:
+        lector_csv = csv.reader(archivo, delimiter=';')
+        for fila in lector_csv:
+            laberinto.append([int(celda) for celda in fila])
 
+    # Encontrar las coordenadas del punto de inicio y la meta
+    for i in range(len(laberinto)):
+        for j in range(len(laberinto[0])):
+            if laberinto[i][j] == 2:
+                inicio = (i, j)
+            elif laberinto[i][j] == 3:
+                meta = (i, j)
 
-def print_maze(maze):
-    for row in maze:
-        print('  '.join(str(cell) for cell in row))
+    return laberinto, inicio, meta
 
+def imprimir_laberinto(laberinto):
+    for fila in laberinto:
+        print(' '.join(str(celda) for celda in fila))
 
-# Cambia 'laberinto.csv' al nombre de tu archivo CSV
-filename = 'Matriz.csv'
-maze = load_maze_from_csv(filename)
+# Cambiar el nombre del archivo por el nombre de tu archivo CSV
+nombre_archivo = 'Matriz.csv'
+laberinto, inicio, meta = cargar_laberinto(nombre_archivo)
 
-print("Laberinto cargado desde el archivo:")
-print_maze(maze)
+print("Laberinto:")
+imprimir_laberinto(laberinto)
+
+print("\nCoordenadas del punto de inicio:", inicio)
+print("Coordenadas del punto final:", meta)
 
 
