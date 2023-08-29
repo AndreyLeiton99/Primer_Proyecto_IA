81898783fe8e2cd6ecb743d92d8ffb9edca1266d
AndreyLeiton99 <aleiton2014@hotmail.com>
2023-08-29 00:50:44 -0600

    Tasks done
    Se "integraron" el CSV, el convertor a matriz y el algoritmo de Dijkstra en el main.
    Así como un menú inicial básico de consola

--- ProyectoIA/Dijkstra.py
+++ ProyectoIA/Dijkstra.py
@@ -5,73 +5,88 @@ import time
 COLORS = ['\033[91m', '\033[92m', '\033[93m', '\033[94m', '\033[95m', '\033[96m', '\033[97m']
 RESET_COLOR = '\033[0m'
 
-def dijkstra_laberinto(laberinto, inicio, meta):
-    n = len(laberinto)
-    m = len(laberinto[0])
+class Dijkstra:
+    def __init__(self, laberinto, inicio, meta):
+        self.laberinto = laberinto
+        self.inicio = inicio
+        self.meta = meta
+        self.n = len(laberinto)
+        self.m = len(laberinto[0])
+        self.distancias = [[float('inf')] * self.m for _ in range(self.n)]
+        self.distancias[inicio[0]][inicio[1]] = 0
+        self.visitados = [[False] * self.m for _ in range(self.n)]
+        self.cola_prioridad = [(0, inicio)]
 
-    distancias = [[float('inf')] * m for _ in range(n)]
-    distancias[inicio[0]][inicio[1]] = 0
-    visitados = [[False] * m for _ in range(n)]
+    def dijkstra_laberinto(self):
+        while self.cola_prioridad:
+            distancia_actual, nodo_actual = heapq.heappop(self.cola_prioridad)
+            x, y = nodo_actual
 
-    cola_prioridad = [(0, inicio)]
+            if nodo_actual == self.meta:
+                break
 
-    while cola_prioridad:
-        distancia_actual, nodo_actual = heapq.heappop(cola_prioridad)
-        x, y = nodo_actual
+            if self.visitados[x][y]:
+                continue
 
-        if nodo_actual == meta:
-            break
+            self.visitados[x][y] = True
 
-        if visitados[x][y]:
-            continue
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                nx, ny = x + dx, y + dy
 
-        visitados[x][y] = True
+                if 0 <= nx < self.n and 0 <= ny < self.m and not self.visitados[nx][ny]:
+                    peso = 1 if self.laberinto[nx][ny] == 0 else float('inf')
+                    nueva_distancia = distancia_actual + peso
 
-        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-            nx, ny = x + dx, y + dy
+                    if nueva_distancia < self.distancias[nx][ny]:
+                        self.distancias[nx][ny] = nueva_distancia
+                        heapq.heappush(self.cola_prioridad, (nueva_distancia, (nx, ny)))
 
-            if 0 <= nx < n and 0 <= ny < m and not visitados[nx][ny]:
-                peso = 1 if laberinto[nx][ny] == 0 else float('inf')
-                nueva_distancia = distancia_actual + peso
+        ruta_minima = self.reconstruir_ruta()
+        return ruta_minima
 
-                if nueva_distancia < distancias[nx][ny]:
-                    distancias[nx][ny] = nueva_distancia
-                    heapq.heappush(cola_prioridad, (nueva_distancia, (nx, ny)))
+    def reconstruir_ruta(self):
+        ruta = []
+        x, y = self.meta
 
-    ruta_minima = reconstruir_ruta(distancias, inicio, meta)
-    return ruta_minima
+        while (x, y) != self.inicio:
+            ruta.append((x, y))
+            vecinos = []
 
+            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < self.n and 0 <= ny < self.m:
+                    vecinos.append(((nx, ny), self.distancias[nx][ny]))
 
-def reconstruir_ruta(distancias, inicio, meta):
-    ruta = []
-    x, y = meta
+            vecinos.sort(key=lambda v: v[1])
 
-    while (x, y) != inicio:
-        ruta.append((x, y))
-        vecinos = []
+            x, y = vecinos[0][0]
 
-        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < len(distancias) and 0 <= ny < len(distancias[0]):
-                vecinos.append(((nx, ny), distancias[nx][ny]))
+        ruta.append(self.inicio)
+        ruta.reverse()
+        return ruta
 
-        vecinos.sort(key=lambda v: v[1])
+    def imprimir_laberinto_con_ruta(self, ruta):
+        for i, fila in enumerate(self.laberinto):
+            for j, celda in enumerate(fila):
+                if (i, j) in ruta:
+                    color_index = ruta.index((i, j)) % len(COLORS)
+                    print(COLORS[color_index] + str(celda) + RESET_COLOR, end=' ')
+                else:
+                    print(str(celda), end=' ')
+            print()
 
-        x, y = vecinos[0][0]
-    
-    ruta.append(inicio)
-    ruta.reverse()
-    return ruta
+    def resolver_laberinto_ejemplo(self):
+            inicio_tiempo = time.time()
+            ruta_minima = self.dijkstra_laberinto()
+            tiempo_total = time.time() - inicio_tiempo
 
-def imprimir_laberinto_con_ruta(laberinto, ruta):
-    for i, fila in enumerate(laberinto):
-        for j, celda in enumerate(fila):
-            if (i, j) in ruta:
-                color_index = ruta.index((i, j)) % len(COLORS)
-                print(COLORS[color_index] + str(celda) + RESET_COLOR, end=' ')
-            else:
-                print(str(celda), end=' ')
-        print()
+            ruta_minima_str = ' -> '.join(f'({x},{y})' for x, y in ruta_minima)
+
+            print("Ruta mínima:", ruta_minima_str)
+            print("Tiempo total:", tiempo_total, "segundos")
+
+            print("\nLaberinto con Ruta:")
+            self.imprimir_laberinto_con_ruta(ruta_minima)
 
 # Crear el laberinto (cambiarlo por tu laberinto real)
 laberinto_ejemplo = [
@@ -85,15 +100,5 @@ laberinto_ejemplo = [
 inicio_ejemplo = (1, 0)
 meta_ejemplo = (4, 4)
 
-inicio_tiempo = time.time()
-ruta_minima = dijkstra_laberinto(
-    laberinto_ejemplo, inicio_ejemplo, meta_ejemplo)
-tiempo_total = time.time() - inicio_tiempo
-
-ruta_minima_str = ' -> '.join(f'({x},{y})' for x, y in ruta_minima)
-
-print("Ruta mínima:", ruta_minima_str)
-print("Tiempo total:", tiempo_total, "segundos")
-
-print("Laberinto con Ruta:")
-imprimir_laberinto_con_ruta(laberinto_ejemplo, ruta_minima)
+laberinto_solver = Dijkstra(laberinto_ejemplo, inicio_ejemplo, meta_ejemplo)
+laberinto_solver.resolver_laberinto_ejemplo()
